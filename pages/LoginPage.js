509cc687edcd9dd6873267dd3c4807238a1fb5df
Mohan S <mohankumar807@gmail.com>
2024-02-12 16:33:46 +1100

    Introduced env file for login url, username, pwd

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,4 +1,5 @@
 const { expect } = require('@playwright/test');
+const { dotenv } = require('dotenv').config();
 class LoginPage {
 
     constructor(page) {
@@ -11,19 +12,16 @@ class LoginPage {
         this.lpabtn = page.locator('.btn-lpa');
         this.accessenvdbtn = page.locator("[value='Access eNVD']").first();
         this.constitle = page.locator('[data-cy=consignment-heading]');
-        this.url = "https://envd.integritysystems.com.au/";
-        this.username = "lpa.user1@yopmail.com";
-        this.password = "Test@1234";
     }
 
     async gotoenvdurl() {
-        await this.page.goto(this.url);
+        await this.page.goto(process.env.ENVD_URL);
     }
 
     async logintoenvd() {
         await this.loginbtn.click();
-        await this.usrname.fill(this.username);
-        await this.pwd.fill(this.password);
+        await this.usrname.fill(process.env.MLA_USERNAME);
+        await this.pwd.fill(process.env.MLA_PASSWORD);
         await this.submitbtn.click();
         await this.lpabtn.click();
         await this.accessenvdbtn.click();
