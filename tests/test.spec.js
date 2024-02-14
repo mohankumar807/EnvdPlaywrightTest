97bee2543c0e617bcf434739dd1638e5df48d167
Mohan S <mohankumar807@gmail.com>
2024-02-14 20:07:55 +1100

    code optimisations

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,33 +2,34 @@ const { test } = require('@playwright/test');
 //import POManager instead of indivdual classes
 const { POManager } = require('../pages/POManager/POManager');
 
-test('TC1- Cattle Consignment', async ({ page }) => {
+test.describe('Consignment Tests', async () => {
+    var pomanager;
 
-    const pomanager = new POManager(page);
-    await pomanager.getloginflow().loginflow();
-    await pomanager.gethomepage().gotocreatefromscratchpage();
-    await pomanager.getfromscratchpage().gotomovementpage();
-    await pomanager.getmovementflow().movementfill();
-    await pomanager.getselectspeciespage().gotoformspage();
-    await pomanager.getformsflow().formse2ecattle();
-    await pomanager.getlivestockflow().livestockcattle();
-    await pomanager.gethistoryflow().historycattle();
-    //page pause
-    await page.pause();
-});
+    test.beforeEach(async ({ page }) => {
+        pomanager = new POManager(page);
+        await pomanager.getloginflow().loginflow();
+        await pomanager.gethomepage().gotocreatefromscratchpage();
+        await pomanager.getfromscratchpage().gotomovementpage();
+        await pomanager.getmovementflow().movementfill();
+    });
 
-test('TC2- SheepLamb', async ({ page }) => {
+    test('TC1- Cattle Consignment', async ({ page }) => {
+        pomanager = new POManager(page);
+        await pomanager.getselectspeciespage().gotoformspage();
+        await pomanager.getformsflow().formse2ecattle();
+        await pomanager.getlivestockflow().livestockcattle();
+        await pomanager.gethistoryflow().historycattle();
+        //page pause
+        await page.pause();
+    });
 
-    const pomanager = new POManager(page);
-    await pomanager.getloginflow().loginflow();
-    await pomanager.gethomepage().gotocreatefromscratchpage();
-    await pomanager.getfromscratchpage().gotomovementpage();
-    await pomanager.getmovementflow().movementfill();
-    //Select species page - sheep/lamb
-    await pomanager.getselectspeciespage().selectspeciessheeplamb();
-    await pomanager.getselectspeciespage().gotoformspage();
-    await pomanager.getformsflow().formse2esheeplamb();
-    await pomanager.getlivestockflow().livestocksheeplamb();
-    //page pause
-    await page.pause();
+    test('TC2- SheepLamb Consignment', async ({ page }) => {
+        pomanager = new POManager(page);
+        await pomanager.getselectspeciespage().selectspeciessheeplamb();
+        await pomanager.getselectspeciespage().gotoformspage();
+        await pomanager.getformsflow().formse2esheeplamb();
+        await pomanager.getlivestockflow().livestocksheeplamb();
+        //page pause
+        await page.pause();
+    });
 });
