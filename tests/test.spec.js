21fc734dcf888b74742080728e30ae9017bb9096
Mohan S <mohankumar807@gmail.com>
2024-02-14 22:54:03 +1100

    Introduced JSDoc Type annotation for object declaration.

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,8 +2,11 @@ const { test } = require('@playwright/test');
 //import POManager instead of indivdual classes
 const { POManager } = require('../pages/POManager/POManager');
 
+
 test.describe('Consignment Tests', async () => {
-    var pomanager;
+    //annotation to let vscode know that pomanager is instance of POManager class
+    /** @type {POManager} */
+    let pomanager;
 
     test.beforeEach(async ({ page }) => {
         pomanager = new POManager(page);
@@ -14,22 +17,18 @@ test.describe('Consignment Tests', async () => {
     });
 
     test('TC1- Cattle Consignment', async ({ page }) => {
-        pomanager = new POManager(page);
         await pomanager.getselectspeciespage().gotoformspage();
         await pomanager.getformsflow().formse2ecattle();
         await pomanager.getlivestockflow().livestockcattle();
         await pomanager.gethistoryflow().historycattle();
-        //page pause
-        await page.pause();
+        // await page.pause();
     });
 
     test('TC2- SheepLamb Consignment', async ({ page }) => {
-        pomanager = new POManager(page);
         await pomanager.getselectspeciespage().selectspeciessheeplamb();
         await pomanager.getselectspeciespage().gotoformspage();
         await pomanager.getformsflow().formse2esheeplamb();
         await pomanager.getlivestockflow().livestocksheeplamb();
-        //page pause
-        await page.pause();
+        // await page.pause();
     });
 });
