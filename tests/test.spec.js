d30715afcd04c64114c2566d516f4bf01f6be5a1
Mohan S <mohankumar807@gmail.com>
2024-02-21 22:04:29 +1100

    Token Access

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,7 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const { POManager } = require('../pages/POManager/POManager');
 
-test.describe('Consignment Tests', async () => {
+test.describe('TS1- Consignment Tests', async () => {
     //annotation to let vscode know that consignments is instance of POManager class
     /** @type { POManager }*/
     let consignments;
@@ -15,6 +15,7 @@ test.describe('Consignment Tests', async () => {
     });
 
     test('TC1- Cattle Consignment', async ({ }) => {
+        test.setTimeout(120000);
         await consignments.getSelectSpeciesPage().gotoFormsPage();
         await consignments.getFormsFlow().fillCattleForms();
         await consignments.getLivestockFlow().fillCattleLivestock();//consignmentid
@@ -23,19 +24,88 @@ test.describe('Consignment Tests', async () => {
     });
 
     test('TC2- SheepLamb Consignment', async ({ }) => {
+        test.setTimeout(120000);
         await consignments.getSelectSpeciesPage().selectSpeciesSheepLamb();
         await consignments.getSelectSpeciesPage().gotoFormsPage();
         await consignments.getFormsFlow().fillsheepforms();
         await consignments.getLivestockFlow().fillSheepLivestock();//consignmentid
     });
 
-    test('TC3- SheepLamb Consignment', async ({ }) => {
-        await consignments.getSelectSpeciesPage().selectSpeciesSheepLamb();
-        await consignments.getSelectSpeciesPage().gotoFormsPage();
-        await consignments.getFormsFlow().fillsheepforms();
-        await consignments.getLivestockFlow().fillSheepLivestock();//consignmentid
+    test.afterEach(async ({ page }) => {
+        // await page.pause();
     });
-    test('TC4- Cattle Consignment', async ({ }) => {
+});
+
+test.describe('TS2- Consignment Storage Session Test', async () => {
+    //annotation to let vscode know that consignments is instance of POManager class
+    /** @type { POManager }*/
+    let consignments;
+    let envdcontext;
+
+    test.beforeAll(async ({ browser }) => {
+        const context = await browser.newContext();
+        let page = await context.newPage();
+        consignments = new POManager(page);
+        await consignments.getLoginFlow().loginFlow();
+        await page.waitForLoadState('networkidle');
+        //save storage state        
+        await context.storageState({ path: 'storageState.json' });
+        //create context and page with storagestate
+        envdcontext = await browser.newContext({ storageState: 'storageState.json' });
+        page = await envdcontext.newPage();
+    });
+
+    test.beforeEach(async () => {
+        const page = await envdcontext.newPage();
+        await page.goto('https://envd.integritysystems.com.au/consignments/');
+        await page.waitForLoadState('networkidle');
+        await consignments.getHomePage().gotoCreateFromScratchPage();
+        await consignments.getFromScratchPage().gotoMovementPage();
+        await consignments.getMovementFlow().fillMovementDetails();
+    });
+
+    test('TC21- Cattle Storage Session', async () => {
+        // tests
+    });
+
+    test('TC22- SheepLamb Storage Session', async () => {
+        // tests
+    });
+
+    test.afterEach(async ({ page }) => {
+        // await page.pause();
+    });
+});
+
+test.describe("Token Access", async () => {
+    //annotation to let vscode know that consignments is instance of POManager class
+    /** @type { POManager }*/
+    let consignments;
+    let token = '';
+
+    test.beforeAll(async ({ browser }) => {
+        if (!token) {
+            const context = await browser.newContext();
+            let page = await context.newPage();
+            consignments = new POManager(page);
+            await consignments.getLoginFlow().loginFlow();
+            await page.waitForLoadState('networkidle');
+            //save token from session storage
+            token = await page.evaluate(() => sessionStorage.getItem('_tokenkey'));
+            console.log('Token is: ', token);
+        }
+    });
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://envd.integritysystems.com.au?t=" + token);
+        await page.waitForLoadState('networkidle');
+
+    });
+
+    test('TC1- Cattle Consignment', async () => {
+        await consignments.getHomePage().gotoCreateFromScratchPage();
+        await consignments.getFromScratchPage().gotoMovementPage();
+        await consignments.getMovementFlow().fillMovementDetails();
         await consignments.getSelectSpeciesPage().gotoFormsPage();
         await consignments.getFormsFlow().fillCattleForms();
         await consignments.getLivestockFlow().fillCattleLivestock();//consignmentid
@@ -43,7 +113,13 @@ test.describe('Consignment Tests', async () => {
         await consignments.getDeclarationFlow().fillDelcarationPage();
     });
 
-    test.afterEach(async ({ page }) => {
-        // await page.pause();
+    test('TC2- SheepLamb Consignment', async () => {
+        await consignments.getHomePage().gotoCreateFromScratchPage();
+        await consignments.getFromScratchPage().gotoMovementPage();
+        await consignments.getMovementFlow().fillMovementDetails();
+        await consignments.getSelectSpeciesPage().selectSpeciesSheepLamb();
+        await consignments.getSelectSpeciesPage().gotoFormsPage();
+        await consignments.getFormsFlow().fillsheepforms();
+        await consignments.getLivestockFlow().fillSheepLivestock();//consignmentid
     });
 });
