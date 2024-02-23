7431fcb2dda0414f21ef3ea3bf0d80363bcc9595
Mohan S <mohankumar807@gmail.com>
2024-02-23 19:44:17 +1100

    Cleanup

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { POManager } = require('../pages/POManager/POManager');
 
-test.describe('TS1- Consignment Tests', async () => {
+test.describe('TS- Consignment Tests', async () => {
     //annotation to let vscode know that consignments is instance of POManager class
     /** @type { POManager }*/
     let consignments;
@@ -31,85 +31,5 @@ test.describe('TS1- Consignment Tests', async () => {
 
     test.afterEach(async ({ page }) => {
         // await page.pause();
-    });
-});
-
-test.describe('TS2- Consignment Storage Session Test', async () => {
-    //annotation to let vscode know that consignments is instance of POManager class
-    /** @type { POManager }*/
-    let consignments;
-    let envdcontext;
-
-    test.beforeAll(async ({ browser }) => {
-        const context = await browser.newContext();
-        let page = await context.newPage();
-        consignments = new POManager(page);
-        await consignments.getLoginFlow().loginFlow();
-        await page.waitForLoadState('networkidle');
-        //save storage state        
-        await context.storageState({ path: 'storageState.json' });
-        //create context and page with storagestate
-        envdcontext = await browser.newContext({ storageState: 'storageState.json' });
-        page = await envdcontext.newPage();
-    });
-
-    test.beforeEach(async () => {
-        const page = await envdcontext.newPage();
-        await page.goto('https://envd.integritysystems.com.au/consignments/');
-        await page.waitForLoadState('networkidle');
-        await consignments.getHomePage().gotoCreateFromScratchPage();
-        await consignments.getFromScratchPage().gotoMovementPage();
-        await consignments.getMovementFlow().fillMovementDetails();
-    });
-
-    test.afterEach(async ({ page }) => {
-        // await page.pause();
-    });
-});
-
-test.describe("Token Access", async () => {
-    //annotation to let vscode know that consignments is instance of POManager class
-    /** @type { POManager }*/
-    let consignments;
-    let token = '';
-
-    test.beforeAll(async ({ browser }) => {
-        if (!token) {
-            const context = await browser.newContext();
-            let page = await context.newPage();
-            consignments = new POManager(page);
-            await consignments.getLoginFlow().loginFlow();
-            await page.waitForLoadState('networkidle');
-            //save token from session storage
-            token = await page.evaluate(() => sessionStorage.getItem('_tokenkey'));
-            console.log('Token is: ', token);
-        }
-    });
-
-    test.beforeEach(async ({ page }) => {
-        await page.goto("https://envd.integritysystems.com.au?t=" + token);
-        await page.waitForLoadState('networkidle');
-
-    });
-
-    test('TC1- Cattle Consignment', async () => {
-        await consignments.getHomePage().gotoCreateFromScratchPage();
-        await consignments.getFromScratchPage().gotoMovementPage();
-        await consignments.getMovementFlow().fillMovementDetails();
-        await consignments.getSelectSpeciesPage().gotoFormsPage();
-        await consignments.getFormsFlow().fillCattleForms();
-        await consignments.getLivestockFlow().fillCattleLivestock();//consignmentid
-        await consignments.getHistoryFlow().fillCattleHistory();
-        await consignments.getDeclarationFlow().fillDelcarationPage();
-    });
-
-    test('TC2- SheepLamb Consignment', async () => {
-        await consignments.getHomePage().gotoCreateFromScratchPage();
-        await consignments.getFromScratchPage().gotoMovementPage();
-        await consignments.getMovementFlow().fillMovementDetails();
-        await consignments.getSelectSpeciesPage().selectSpeciesSheepLamb();
-        await consignments.getSelectSpeciesPage().gotoFormsPage();
-        await consignments.getFormsFlow().fillsheepforms();
-        await consignments.getLivestockFlow().fillSheepLivestock();//consignmentid
     });
 });
